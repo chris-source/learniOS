e3ec5e071eedf3afb19e717a05a8508b9cbe7146
mexiqq <lijianwei@139.me>
2014-09-10 20:04:42 +0800

    Initial Commit

---
+++ RecipeBookMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RecipeBookMe
+//
+//  Created by liwenqian on 14-9-10.
+//  Copyright (c) 2014年 liwenqian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RecipeBookMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RecipeBookMe
+//
+//  Created by liwenqian on 14-9-10.
+//  Copyright (c) 2014年 liwenqian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
